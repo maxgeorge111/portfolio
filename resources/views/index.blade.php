95ea0ea2738856bdfdafd38d8ef88dd5a34d8d46
Desktop/Max <maxgeorge111@hotmail.com>
2024-05-09 14:14:47 +0100

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<title>Max Hogan - Portfolio</title>
+
+		<link href="{{ url('css/bootstrap.min.css') }}" rel="stylesheet">
+
+		<link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+		<link href="{{ url('css/font-awesome.min.css') }}" rel="stylesheet">
+		<link href="{{ url('css/devicons.min.css') }}" rel="stylesheet">
+		<link href="{{ url('css/simple-line-icons.css') }}" rel="stylesheet">
+
+		<link href="{{ url('css/resume.min.css') }}" rel="stylesheet">
+	</head>
+
+	<body id="page-top">
+		<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+			<a class="navbar-brand js-scroll-trigger" href="#page-top">
+				<span class="d-block d-lg-none">Portfolio</span>
+				<span class="d-none d-lg-block">
+					<img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profileweb.png" alt="">
+				</span>
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link js-scroll-trigger" href="#about">About</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link js-scroll-trigger" href="#experience">Experience</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link js-scroll-trigger" href="#education">Education</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link js-scroll-trigger" href="#skills">Skills</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link js-scroll-trigger" href="#interests">Interests</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link js-scroll-trigger" href="#awards">Awards</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+
+		<div class="container-fluid p-0">
+			<section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+				<div class="my-auto">
+					<h1 class="mb-0">Max
+						<span class="text-primary">Hogan</span>
+					</h1>
+					<div class="subheading mb-5">8 Ruthin Walk · Helsby, WA6 0EE · 07465447795 ·
+						<a href="mailto:name@email.com">maxhogan1@hotmail.com</a>
+					</div>
+					<p class="mb-5">Driven Software Engineering graduate with professional experience in developing software features for deployment into enterprise level IT systems. Enjoys working out how code can be used to solve problems as a hobby and wants to turn that interest into a career that poses new challenges every day.</p>
+					<ul class="list-inline list-social-icons mb-0">
+						<li class="list-inline-item">
+							<a href="https://www.linkedin.com/in/max-hogan-099378195/">
+								<span class="fa-stack fa-lg">
+									<i class="fa fa-circle fa-stack-2x"></i>
+									<i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+								</span>
+							</a>
+						</li>
+						<li class="list-inline-item">
+							<a href="https://github.com/maxgeorge111">
+								<span class="fa-stack fa-lg">
+									<i class="fa fa-circle fa-stack-2x"></i>
+									<i class="fa fa-github fa-stack-1x fa-inverse"></i>
+								</span>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</section>
+
+			<section class="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
+				<div class="my-auto">
+					<h2 class="mb-5">Experience</h2>
+
+					<div class="resume-item d-flex flex-column flex-md-row mb-5">
+						<div class="resume-content mr-auto">
+							<h3 class="mb-0">Software Developer</h3>
+							<div class="subheading mb-3">The Walton Centre - NHS Foundation Trust</div>
+							<ul>
+								<li>Worked to digitise hospital systems by creating web tools and online forms using PHP, HTML and JavaScript.</li>
+								<li>Developed a system for booking staff leave.</li>
+								<li>Managed a database using advanced SQL stored procedures.</li>
+								<li>Created and ran test scripts for testing code changes.</li>
+								<li>Worked within task deadlines to prevent release delays.</li>
+								<li>Observed industry best practices to produce readable, efficient code.</li>
+								<li>Participated in code reviews ensuring code correctly followed organisation standards.</li>
+							</ul>
+						</div>
+						<div class="resume-date text-md-right">
+							<span class="text-primary">August 2022 - August 2023</span>
+						</div>
+					</div>
+
+					<div class="resume-item d-flex flex-column flex-md-row mb-5">
+						<div class="resume-content mr-auto">
+							<h3 class="mb-0">IT Technician Intern</h3>
+							<div class="subheading mb-3">Helsby High School</div>
+							<ul>
+								<li>Troubleshot various incidents related to hardware, networks and software faults.</li>
+								<li>Regularly maintained projectors, desktop computers and laptops.</li>
+								<li>Responded to internal support tickets from staff members for technical support.</li>
+							</ul>
+						</div>
+						<div class="resume-date text-md-right">
+							<span class="text-primary">April 2019 - January 2020</span>
+						</div>
+					</div>
+
+					<div class="resume-item d-flex flex-column flex-md-row mb-5">
+						<div class="resume-content mr-auto">
+							<h3 class="mb-0">Shop Assistant</h3>
+							<div class="subheading mb-3">WHSmith</div>
+							<ul>
+								<li>Covered extra shifts and maintained a flexible schedule to achieve store goals.</li>
+								<li>Built positive relations with customers and fellow staff members.</li>
+							</ul>
+						</div>
+						<div class="resume-date text-md-right">
+							<span class="text-primary">October 2017 - August 2020</span>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="resume-section p-3 p-lg-5 d-flex flex-column" id="education">
+				<div class="my-auto">
+					<h2 class="mb-5">Education</h2>
+					<div class="resume-item d-flex flex-column flex-md-row mb-5">
+						<div class="resume-content mr-auto">
+							<h3 class="mb-0">Edge Hill University</h3>
+							<div class="subheading mb-3">Bachelor of Science (Hons)</div>
+							<div>Software Engineering</div>
+							<p>Waiting for result</p>
+						</div>
+						<div class="resume-date text-md-right">
+							<span class="text-primary">September 2020 - May 2024</span>
+						</div>
+					</div>
+
+					<div class="resume-item d-flex flex-column flex-md-row">
+						<div class="resume-content mr-auto">
+							<h3 class="mb-0">Cronton Sixth Form College</h3>
+							<div class="subheading mb-3">IT Level 3 BTEC</div>
+							<p>Achieved D*, D*, D*</p>
+						</div>
+						<div class="resume-date text-md-right">
+							<span class="text-primary">September 2018 - July 2020</span>
+						</div>
+					</div>
+
+					<div class="resume-item d-flex flex-column flex-md-row">
+						<div class="resume-content mr-auto">
+							<h3 class="mb-0">Helsby High School</h3>
+							<div class="subheading mb-3">GCSE</div>
+							<p>9 GCSE's including Maths, English, and Science. Computer Science at grade 8.</p>
+						</div>
+						<div class="resume-date text-md-right">
+							<span class="text-primary">September 2013 - July 2018</span>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
+				<div class="my-auto">
+					<h2 class="mb-5">Skills</h2>
+					<div class="subheading mb-3">Programming Languages</div>
+					<ul class="list-inline list-icons">
+						<li class="list-inline-item">
+							<i class="devicons devicons-java"></i>
+						</li>
+						<li class="list-inline-item">
+							<i class="devicons devicons-javascript"></i>
+						</li>
+						<li class="list-inline-item">
+							<i class="devicons devicons-jquery"></i>
+						</li>
+						<li class="list-inline-item">
+							<i class="devicons devicons-php"></i>
+						</li>
+						<li class="list-inline-item">
+							<i class="devicons devicons-html5"></i>
+						</li>
+						<li class="list-inline-item">
+							<i class="devicons devicons-python"></i>
+						</li>
+					</ul>
+					<div class="subheading mb-3">Tools</div>
+					<ul class="list-inline list-icons">
+						<li class="list-inline-item">
+							<i class="devicons devicons-git"></i>
+						</li>
+						<li class="list-inline-item">
+							<i class="devicons devicons-laravel"></i>
+						</li>
+						<li class="list-inline-item">
+							<i class="devicons devicons-bootstrap"></i>
+						</li>
+						<li class="list-inline-item">
+							<i class="devicons devicons-mysql"></i>
+						</li>
+						<li class="list-inline-item">
+							<i class="devicons devicons-firebase"></i>
+						</li>
+						<li class="list-inline-item">
+							<i class="devicons devicons-jira"></i>
+						</li>
+					</ul>
+
+					<div class="subheading mb-3">Workflow</div>
+					<ul class="fa-ul mb-0">
+						<li>
+							<i class="fa-li fa fa-check"></i>
+							Version Control Systems</li>
+						<li>
+							<i class="fa-li fa fa-check"></i>
+							Testing &amp; Debugging</li>
+						<li>
+							<i class="fa-li fa fa-check"></i>
+							Jira Product Suite</li>
+						<li>
+							<i class="fa-li fa fa-check"></i>
+							Agile Development &amp; Scrum</li>
+					</ul>
+				</div>
+			</section>
+
+			<section class="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
+				<div class="my-auto">
+					<h2 class="mb-5">Interests</h2>
+					<p>In addition to my passion for software development, I have a general interest in all things tech especially in keeping up with new hardware releases that keep pushing the limits of whats possible. If I'm inside I'll usually be playing Rocket League and speaking to friends on Discord or listening to Spotify.</p>
+				</div>
+			</section>
+
+			<section class="resume-section p-3 p-lg-5 d-flex flex-column" id="awards">
+				<div class="my-auto">
+					<h2 class="mb-5">Awards &amp; Certifications</h2>
+					<ul class="fa-ul mb-0">
+						<li>
+							<i class="fa-li fa fa-trophy text-warning"></i>
+							Microsoft Certified: Azure AI Fundamentals</li>
+						<li>
+							<i class="fa-li fa fa-trophy text-warning"></i>
+							Completed the Artificial Intelligence Winter Program with Asia University
+						</li>
+						<li>
+							<i class="fa-li fa fa-trophy text-warning"></i>
+							Took part in the Solstice Fire &amp; Ice Game Jam hosted by the University of Alaska
+						</li>
+						<li>
+							<i class="fa-li fa fa-trophy text-warning"></i>
+							Completed a cyber security course for CyberFirst at Newcastle University 
+						</li>
+					</ul>
+				</div>
+			</section>
+
+		</div>
+
+		<!-- Bootstrap core JavaScript -->
+		<script src="{{url('js/jquery.min.js') }}"></script>
+		<script src="{{url('js/bootstrap.bundle.min.js') }}"></script>
+
+		<!-- Plugin JavaScript -->
+		<script src="{{url('js/jquery.easing.min.js') }}"></script>
+
+		<!-- Custom scripts for this template -->
+		<script src="{{url('js/resume.min.js') }}"></script>
+
+	</body>
+
+</html>
